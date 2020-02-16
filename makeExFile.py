6746148bafdce335486e1c86235c2bbdc601730f
TKjapan <t2.koseki@gmail.com>
2020-02-16 12:49:25 +0900

    change

--- makeExFile.py
+++ makeExFile.py
@@ -8,7 +8,7 @@ from openpyxl.chart import LineChart, Reference, Series
 num = 0
 threshold_el = 1
 threshold_az = 0.3
-A2Cnum = list()
+ProdactSirialnum = list()
 sOrgPath = os.getcwd()
 
 #make excel fail
@@ -17,15 +17,20 @@ sheet1 = wb1.create_sheet(title = "data_el")
 sheet2 = wb1.create_sheet(title = "data_Az")
 sheet3 = wb1.create_sheet(title = "summry")
 
-#conti result excel open
-wb2 = xl.load_workbook("test.xlsx")
-sheet_name = wb2.get_sheet_names()
-sheet = wb2.get_sheet_by_name(str(sheet_name))
-
 #get csv file list
 path_obj = Path()
 
 csvfile = list(path_obj.glob("*.csv"))
+
+#get xlsx file list
+xlsxfilelist = list(path_obj.glob("*.xlsx"))
+
+if xlsxfilelist.count("testresult.xlsx")!=0:
+
+    #test result excel open
+    wb2 = xl.load_workbook(str(xlsxfilelist[xlsxfilelist.index("testresult.xlsx")]))
+    sheet_name = wb2.get_sheet_names()
+    sheet = wb2.get_sheet_by_name(str(sheet_name))
 
 #copy az and el data form csvfile to ExcelFile
 for listnum in  range(len(csvfile)):
@@ -36,15 +41,15 @@ for listnum in  range(len(csvfile)):
     CsvData = pd.read_csv(str(csvfile[listnum]),encoding="SHIFT-JIS", skiprows=2)
     
     #同一のA2C番号があった場合は、その行に書き込む。ない場合は、新しい行に書き込む。
-    if A2Cnum.count(name[15:23])==0:
-        #write A2Cnum to xlsx
-        A2Cnum.append(name[15:23])
-        num = len(A2Cnum)
-        sheet1.cell(row=1, column=num+1).value = A2Cnum[num-1] 
-        sheet2.cell(row=1, column=num+1).value = A2Cnum[num-1] 
+    if ProdactSirialnum.count(name[15:23])==0:
+        #write ProdactSirialnum to xlsx
+        ProdactSirialnum.append(name[15:23])
+        num = len(ProdactSirialnum)
+        sheet1.cell(row=1, column=num+1).value = ProdactSirialnum[num-1] 
+        sheet2.cell(row=1, column=num+1).value = ProdactSirialnum[num-1] 
      
     else:
-        num=A2Cnum.index(name[15:23])+1
+        num=ProdactSirialnum.index(name[15:23])+1
 
     for p in range(len(CsvData['Error-P'])):
         
